f4a51ef28dc3c3ae03795d15b0bbc770c8b9be50
Chuck Aude <21095583+chuckaude@users.noreply.github.com>
2026-09-02 16:59:35 -0700

    update file src/main/java/SqlInjection.java

--- src/main/java/SqlInjection.java
+++ src/main/java/SqlInjection.java
@@ -6,9 +6,10 @@ import javax.servlet.http.HttpServletRequest;
 public class SqlInjection {
     public static ResultSet doQuery(HttpServletRequest request, Connection connection) throws SQLException {
         String customerName = request.getParameter("customerName");
-        String query = "SELECT account_balance FROM user_data WHERE user_name = " + customerName;
-        Statement statement = connection.createStatement();
-        ResultSet results = statement.executeQuery(query);
+        String query = "SELECT account_balance FROM user_data WHERE user_name = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, customerName);
+        ResultSet results = statement.executeQuery();
         return(results);
     }
 }
